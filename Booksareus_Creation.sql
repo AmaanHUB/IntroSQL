d15fe4aaa703cb3285b690b488d212d3045834aa
Amaan Hashmi-Ubhi <AmaanHUB@gmail.com>
2020-10-26 17:22:12 +0000

    SQL Creation add, no foreign keys, just tables

---
+++ Booksareus_Creation.sql
@@ -0,0 +1,25 @@
+CREATE DATABASE booksareus;
+
+CREATE TABLE user(
+    user_id INT NOT NULL IDENTITY PRIMARY KEY,
+    email VARCHAR(20),
+    phone_number VARCHAR(11),
+    forename VARCHAR(20),
+    surname VARCHAR(20)
+);
+
+
+CREATE TABLE eBook(
+    ebook_id INT NOT NULL IDENTITY PRIMARY KEY,
+    title VARCHAR(20),
+    book_location VARCHAR(20),
+    release_data DATE,
+    author VARCHAR(20)
+);
+
+CREATE TABLE loan(
+    transaction_id INT NOT NULL IDENTITY PRIMARY KEY,
+    loan_date DATE,
+    price DECIMAL(5,2)
+    -- probably a foreign key here for user
+)
