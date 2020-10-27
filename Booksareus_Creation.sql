3502659197cb16875e729c63b00307b8a7e0068a
Amaan Hashmi-Ubhi <AmaanHUB@gmail.com>
2020-10-27 11:35:19 +0000

    Changed author_id

--- Booksareus_Creation.sql
+++ Booksareus_Creation.sql
@@ -1,29 +1,24 @@
 CREATE DATABASE booksareus;
-
 USE booksareus;
-
 CREATE TABLE users(
-    user_id INT NOT NULL IDENTITY PRIMARY KEY,
-    email VARCHAR(20),
-    phone_number VARCHAR(11),
-    forename VARCHAR(20),
-    surname VARCHAR(20)
+  user_id INT NOT NULL IDENTITY PRIMARY KEY,
+  email VARCHAR(20),
+  phone_number VARCHAR(11),
+  forename VARCHAR(20),
+  surname VARCHAR(20)
 );
-
-
 CREATE TABLE eBook(
-    ebook_id INT NOT NULL IDENTITY PRIMARY KEY,
-    title VARCHAR(20),
-    book_location VARCHAR(20),
-    release_date DATE,
-	book_summary VARCHAR(MAX) NOT NULL,
-    author VARCHAR(20)
+  ebook_id INT NOT NULL IDENTITY PRIMARY KEY,
+  title VARCHAR(20),
+  book_location VARCHAR(20),
+  release_date DATE,
+  book_summary VARCHAR(MAX) NOT NULL,
+  author INT REFERENCES users(user_id)
 );
-
 CREATE TABLE loan(
-    transaction_id INT NOT NULL IDENTITY PRIMARY KEY,
-    loan_date DATE,
-    price DECIMAL(5,2),
-    customer_id INT REFERENCES users(user_id),
-    eBook_id INT REFERENCES eBook(ebook_id)
+  transaction_id INT NOT NULL IDENTITY PRIMARY KEY,
+  loan_date DATE,
+  price DECIMAL(5, 2),
+  customer_id INT REFERENCES users(user_id),
+  eBook_id INT REFERENCES eBook(ebook_id)
 );
